002a9554915580853f152d042e0c171835759719
mattwellman812 <williamwellman98@gmail.com>
2020-08-04 16:20:37 -0400

    no message

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,12 +1,9 @@
-
-
-
 const authReducer = (state = {}, action) => {
 	switch (action.type) {
 		case 'GET_SIGNED_IN_USER_ID':
 			return { ...state, getCurrentSignedInUserId: action.getCurrentSignedInUserId }
 		default:
-			 return state
+			return state
 	}
 }
 
